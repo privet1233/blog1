5a5fc060f3bf82187b1fccce5a0a059c54c715a0
Igor <rudnev.2014@inbox.ru>
2023-07-10 10:56:40 +0300

    ПЕрвая загрука

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+
+
+class Comment < ApplicationRecord
+  include Visible
+
+  belongs_to :article
+end
+
+
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+
+Rails.application.routes.draw do
+  #get 'pages/home'
+  root "articles#index" 
+
+ devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' },         controllers: {registration: 'users/registration'}
+
+  
+  
+  resources :articles do
+    resources :comments
+  end
+end
+
+
+
+
